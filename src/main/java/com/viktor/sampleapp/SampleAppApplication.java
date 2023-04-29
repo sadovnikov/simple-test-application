8d4916068904c72ba86df0d5009ebdcc9758b0c4
Viktor Sadovnikov <viktor@jv-ration.com>
2023-04-29 05:58:43 +0200

    Initial version

--- src/main/java/com/viktor/sampleapp/SampleAppApplication.java
+++ src/main/java/com/viktor/sampleapp/SampleAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class SampleAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleAppApplication.class, args);
